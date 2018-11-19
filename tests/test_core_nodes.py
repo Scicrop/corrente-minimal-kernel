08919bdfc4d646e4df76644e0a2a29868e3bff86
David Kwast <david@kwast.net>
2018-11-18 23:23:50 -0200

    variable name clash in a test

--- tests/test_core_nodes.py
+++ tests/test_core_nodes.py
@@ -16,17 +16,17 @@ def test_node_transaction_simple():
         node_producer.process_hash_chain()
     with freeze_time(datetime(2018,6,1,9,2, tzinfo=timezone.utc)):
         node_transporter_source.process_hash_chain()
-    transaction_transport = core.TransactionNode(
+    transaction_transport_source = core.TransactionNode(
         'TRANSPORT-SOURCE',
         node_producer.hash_chain__object.make_playload(),
         node_transporter_source.hash_chain__object.make_playload(),
     )
     with freeze_time(datetime(2018,6,1,9,3, tzinfo=timezone.utc)): # todo: use timestamp, maybe
-        transaction_transport.process_source_signature() # todo: source key
+        transaction_transport_source.process_source_signature() # todo: source key
     with freeze_time(datetime(2018,6,1,9,4, tzinfo=timezone.utc)): # todo: use timestamp, maybe
-        transaction_transport.process_target_signature() # todo: target key
+        transaction_transport_source.process_target_signature() # todo: target key
     with freeze_time(datetime(2018,6,1,9,5, tzinfo=timezone.utc)):
-        transaction_transport.process_hash_chain()
+        transaction_transport_source.process_hash_chain()
     
     # connect transporter node with consumer node
     node_transporter_target = core.DataNode(8364, {
@@ -38,17 +38,17 @@ def test_node_transaction_simple():
         node_transporter_target.process_hash_chain()
     with freeze_time(datetime(2018,6,1,14,2, tzinfo=timezone.utc)):
         node_consumer.process_hash_chain()
-    transaction_transport = core.TransactionNode(
+    transaction_transport_target = core.TransactionNode(
         'TRANSPORT-TARGET',
         node_transporter_target.hash_chain__object.make_playload(),
         node_consumer.hash_chain__object.make_playload(),
     )
     with freeze_time(datetime(2018,6,1,14,3, tzinfo=timezone.utc)): # todo: use timestamp, maybe
-        transaction_transport.process_source_signature() # todo: source key
+        transaction_transport_target.process_source_signature() # todo: source key
     with freeze_time(datetime(2018,6,1,14,4, tzinfo=timezone.utc)): # todo: use timestamp, maybe
-        transaction_transport.process_target_signature() # todo: target key
+        transaction_transport_target.process_target_signature() # todo: target key
     with freeze_time(datetime(2018,6,1,14,5, tzinfo=timezone.utc)):
-        transaction_transport.process_hash_chain()
+        transaction_transport_target.process_hash_chain()
     
     # connect source transport node with target transport node
     
